58f09ca2e0f0faa701b5c2a201b3bc7bc1ff37aa
Shalom Sanevich <shalom.sanevich@mavenir.com>
2020-11-09 16:19:31 +0200

    fix controller

--- src/main/java/com/sanevich/api/DownloadController.java
+++ src/main/java/com/sanevich/api/DownloadController.java
@@ -3,13 +3,12 @@ package com.sanevich.api;
 import com.sanevich.service.ZipDownloadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,11 +16,12 @@ public class DownloadController {
     private final ZipDownloadService zipDownloadService;
 
     @GetMapping("batchdownload/{num}")
-    public ResponseEntity<StreamingResponseBody> downloadAsZip(final HttpServletResponse response, @PathVariable int num) {
-        response.setContentType("application/zip");
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"some.zip\"");
+    public ResponseEntity<StreamingResponseBody> downloadAsZip(@PathVariable int num) {
         var srb = zipDownloadService.downloadAsZip(num);
-        return ResponseEntity.ok(srb);
+        return ResponseEntity.status(200)
+                .contentType(MediaType.parseMediaType("application/zip"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"some.zip\"")
+                .body(srb);
     }
 
 }
